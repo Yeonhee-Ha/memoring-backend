69ee514750e52a055af07ec20ba8f73e611631d3
Yeonhee-Ha <flfkrdl123@gmail.com>
2025-08-05 11:09:48 +0900

    [#1] setting : swagger 연결

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
-# Create your views here.
+#class HelloView(APIView):
+
+    #@swagger_auto_schema(
+    #    operation_description="GET 요청 예시입니다.",
+    #    responses={200: openapi.Response('성공')},
+    #)
+#    def get(self, request):
+#        return Response({"message": "Hello, world!"})
