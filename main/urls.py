4dc646e039f3779198f0bbfc8d8bd9299b21f389
Yeonhee-Ha <flfkrdl123@gmail.com>
2025-08-05 14:18:31 +0900

    [#1] style: urls.py 주석처리

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('hello/', HelloView.as_view(), name='hello'),
+    # path('hello/', HelloView.as_view(), name='hello'),
 ]
