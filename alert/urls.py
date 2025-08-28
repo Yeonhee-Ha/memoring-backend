0ee94e62a20db87081f5b5bb21d19b2db5a6dd39
junh5ng <wnsghd1130@dgu.ac.kr>
2025-08-28 14:37:49 +0900

    [Feat] alert/views.py CRUD/pending/send/send_due 구현, alert/urls.py router 연결

--- alert/urls.py
+++ alert/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import AlertViewSet
 from . import views
 #from views import * 
 
 app_name = "alert" 
 
+router = DefaultRouter()
+router.register("", AlertViewSet, basename="alert")
+
 urlpatterns = [
+  path("", include(router.urls)),
 ]
